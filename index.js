0755f4665b1dc4a7c72e7dc56bfb2acd9dc8dad7
Cosmic-rare <ysolakey1116@gmail.com>
2021-09-16 20:05:22 +0900

    コメントアウト&elseの削除

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Discord, Client, Intents, MessageButton, MessageActionRow } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const hash = require('./sample.js');
+const hash = require('./sample.js'); // hash作成関数の読み込み
 
 client.on('ready', () => {
     console.log(`${client.user.tag} でログインしています。`)
@@ -12,6 +12,7 @@ client.on('messageCreate', async message => {
             .setCustomId("contact")
             .setStyle("PRIMARY")
             .setLabel("🎫問い合わせ🎫")
+        // ボタンを表示する
         await message.channel.send({
             content: "お問い合わせはこちらから",
             components: [new MessageActionRow().addComponents(tic1)]
@@ -20,22 +21,21 @@ client.on('messageCreate', async message => {
         message.channel.send('Pong!');
     } else if (message.content.startsWith('!github')){
         message.channel.send('https://github.com/Cosmic-rare');
-    } else {
-        if(message.author.bot)　return;
-        message.channel.send('その他');
     }
 });
 
 client.on('interactionCreate', async (interaction) => {
     if (interaction.customId === "contact") {
+        // hash生成
         data = hash(interaction.user.id);
         await interaction.reply({
+            // 生成したhashをurlに埋め込む
             content: `http://localhost:5000/${interaction.user.id}/${data}`,
             ephemeral: false
         });
     }
 });
 
-require('dotenv').config();
+require('dotenv').config(); // 読み込んでログイン
 
 client.login(process.env.TOKEN)
